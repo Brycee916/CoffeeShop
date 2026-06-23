ba959b27c87b5a34034827ca0cd5e339757c762e
brycee916 <brycechao27@gmail.com>
2026-06-22 22:31:49 -0700

    added api to get product by id

--- src/main/java/com/coffeeshop/menu/controller/ProductController.java
+++ src/main/java/com/coffeeshop/menu/controller/ProductController.java
@@ -1,17 +1,19 @@
 package com.coffeeshop.menu.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
 import com.coffeeshop.menu.model.Product;
 
 @Controller
 @RequestMapping("/products") // This means all URLs start with http://localhost:8080/products/
 public class ProductController {
 
-    private List<Product> productsList= List.of(
+    private final List<Product> productsList= List.of(
         new Product(1, "Expresso", 2.50),
         new Product(2, "Latte", 3.50),
         new Product(3, "Croissant", 2.00),
@@ -33,6 +35,18 @@ public class ProductController {
             productDisplay += "Product: " + product.getId() + " - " + product.getName() + " - $" + product.getPrice() + "<br>";
         }
         return productDisplay;
+    }
+
+    @RequestMapping("/details/{id}")
+    @ResponseBody
+    public String getProductDetailsById(@PathVariable int id){
+        for (Product product: productsList){
+            if (product.getId() == id){
+                return "Product: " + product.getId() + " - " + product.getName() + " - $" + product.getPrice();
+            }
+        }
+        // couldn't find the product
+        return "Product does not exist";
     }
 
 }
