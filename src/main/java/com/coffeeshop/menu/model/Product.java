18237dd9b2034f2c7c6d09a61554fa44f5b12a54
brycee916 <brycechao27@gmail.com>
2026-07-02 23:21:20 -0700

    imported more dependencies and using jpa in model

--- src/main/java/com/coffeeshop/menu/model/Product.java
+++ src/main/java/com/coffeeshop/menu/model/Product.java
@@ -1,14 +1,23 @@
 package com.coffeeshop.menu.model;
 
-import jakarta.annotation.Nonnull;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 
-
+@Entity
+@Table(name="Products")
 public class Product {
-    @Nonnull
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
-    @Nonnull
+
+    @NotNull(message="Product name cannot be null")
     private String name;
-    @Nonnull
+    
 
     private double price;
 
